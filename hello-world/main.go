9d9b0d2727064b473cdc4536226009632e220324
renato@likwi.com.br <renatofagalde>
2024-04-22 19:56:05 -0700

    deploy 01

--- hello-world/main.go
+++ hello-world/main.go
@@ -18,7 +18,7 @@ func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyRespo
 	}
 
 	return events.APIGatewayProxyResponse{
-		Body:       fmt.Sprintf("%s%s", greeting, " 😊"),
+		Body:       fmt.Sprintf("%s%s", greeting, " 😊🐱‍🚀"),
 		StatusCode: 200,
 	}, nil
 }
