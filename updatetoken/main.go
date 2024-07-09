455353576cacd04e49418d2d2e6ca2a8bba85ad8
renato@likwi.com.br <renatofagalde>
2024-07-09 16:36:49 -0700

    cross 07

--- updatetoken/main.go
+++ updatetoken/main.go
@@ -19,6 +19,7 @@ var (
 )
 
 func handleRequest(ctx context.Context) (string, error) {
+	fmt.Println("cross-07")
 	// Conectar ao banco de dados
 	dsn := fmt.Sprintf("%s:%s@tcp(%s)/%s", dbUser, dbPassword, dbHost, dbName)
 	db, err := sql.Open("mysql", dsn)
