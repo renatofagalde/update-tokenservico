0e65a9e53c33b4c6851d77f996b4ef395b831b0c
renato@likwi.com.br <renatofagalde>
2024-07-10 07:34:13 -0700

    root 02-A

--- main.go
+++ main.go
@@ -19,7 +19,7 @@ var (
 )
 
 func handleRequest(ctx context.Context) (string, error) {
-	fmt.Println("direto no root 03")
+	fmt.Println("direto no root 02 - A")
 	// Conectar ao banco de dados
 	dsn := fmt.Sprintf("%s:%s@tcp(%s)/%s", dbUser, dbPassword, dbHost, dbName)
 	db, err := sql.Open("mysql", dsn)
