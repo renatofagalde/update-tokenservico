4dafedfbef3fb13fb3c444875d4ef76e0be7c531
renato@likwi.com.br <renatofagalde>
2024-07-10 10:30:30 -0700

    makefile 06

--- main.go
+++ main.go
@@ -19,7 +19,7 @@ var (
 )
 
 func handleRequest(ctx context.Context) (string, error) {
-	fmt.Println("05")
+	fmt.Println("06")
 	// Conectar ao banco de dados
 	dsn := fmt.Sprintf("%s:%s@tcp(%s)/%s", dbUser, dbPassword, dbHost, dbName)
 	db, err := sql.Open("mysql", dsn)
